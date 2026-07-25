ced4c3e5d088d64f24ede0883bc3a5681839649b
Bhav1812 <bhavya1812@gmail.com>
2026-07-25 22:07:20 +0530

    Delete Week-5/Angular/student-course-portal directory

--- Week-5/Angular/student-course-portal/src/app/features/enrollment/enrollment.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EnrollmentRoutingModule } from './enrollment-routing.module';
-
-@NgModule({
-  imports: [EnrollmentRoutingModule]
-})
-export class EnrollmentModule {}
